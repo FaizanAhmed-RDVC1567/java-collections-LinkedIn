21f55fdac9712d4cd87b7eb03d8bbcbf6b5612f7
FaizanA <chfaizan.510@outlook.com>
2025-05-28 08:49:41 +0100

    Added abstract method for implementing in a child class.

--- JavaCollections_ch2-1b/src/main/java/com/linkedIn_learning/collections/ExtendedContract.java
+++ JavaCollections_ch2-1b/src/main/java/com/linkedIn_learning/collections/ExtendedContract.java
@@ -1,4 +1,11 @@
 package com.linkedIn_learning.collections;
 
 public interface ExtendedContract {
+    // The final part of the section concerns the topic of inheritance. An interface can extend another
+    // interface. When that happens, and a developer specifies an abstract method in the super (interface),
+    // it must be implemented in the implementation that implements the base interface that inherited the
+    // parent interface. If that does not happen,the IDE/compiler will again complain about the conditions
+    // for the 'contract' not being met.
+
+    public void extendedTerm();
 }
